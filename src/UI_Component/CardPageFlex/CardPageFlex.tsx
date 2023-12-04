4ddfbd67935f38b8ff2c041c5e043c4518aace61
ElkaMaclaud <elkamaclaud@gmail.com>
2023-12-04 19:34:43 +0300

    Added dinamic calculation coordinates in CardPageFlex

--- src/UI_Component/CardPageFlex/CardPageFlex.tsx
+++ src/UI_Component/CardPageFlex/CardPageFlex.tsx
@@ -1,4 +1,11 @@
-import React, { FC, ReactNode, CSSProperties } from "react";
+import React, {
+  FC,
+  ReactNode,
+  CSSProperties,
+  useRef,
+  useState,
+  useLayoutEffect,
+} from "react";
 import classes from "./style/CardPageFlex.module.css";
 
 export const CardPageFlex: FC<{
@@ -6,6 +13,31 @@ export const CardPageFlex: FC<{
   maxWidth?: number;
   style?: CSSProperties;
 }> = ({ children, style, maxWidth }) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const refParent = useRef<HTMLDivElement>(null);
+  const [styles, setStyles] = useState<CSSProperties>({
+    left: 0,
+    right: 0,
+    width: 0,
+    maxWidth: 0,
+  });
+  useLayoutEffect(() => {
+    if (ref.current && refParent.current) {
+      const width =
+        refParent.current?.clientWidth - ref.current?.clientWidth - 30;
+      const left =
+        ref.current?.offsetLeft +            //.getBoundingClientRect().x
+        ref.current?.clientWidth +
+        30;
+        const right = window.innerWidth - refParent.current?.clientWidth;
+      setStyles(() => ({
+        left: left,
+        right: right,
+        width: width,
+        maxWidth: width,
+      }));
+    }
+  }, []);
   if (Array.isArray(children)) {
     if (children.length === 3) {
       return (
@@ -15,11 +47,13 @@ export const CardPageFlex: FC<{
             className={classes.contentWrapper}
           >
             <div className={classes.contentHeader}>{children[0]}</div>
-            <div className={classes.content}>
-              <div className={classes.leftContent} style={style}>
+            <div ref={refParent} className={classes.content}>
+              <div ref={ref} className={classes.leftContent} style={style}>
                 {children[1]}
               </div>
-              <div className={classes.rightContent}>{children[2]}</div>
+              <div style={styles} className={classes.rightContent}>
+                {children[2]}
+              </div>
             </div>
           </div>
         </div>
