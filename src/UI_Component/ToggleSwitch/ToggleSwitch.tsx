25ea37e524a273f959b6987e7bbc9159d0f0cf8e
ElkaMaclaud <elkamaclaud@gmail.com>
2023-12-07 15:31:56 +0300

    Typing ToggleSwitch event

--- src/UI_Component/ToggleSwitch/ToggleSwitch.tsx
+++ src/UI_Component/ToggleSwitch/ToggleSwitch.tsx
@@ -1,10 +1,10 @@
-import React, { FC } from "react";
+import React, { ChangeEvent, FC } from "react";
 import classes from "./style/ToggleSwitch.module.css";
 
 export const ToggleSwitch: FC<{check?: boolean; setCheck: React.Dispatch<React.SetStateAction<boolean>>;
 }> = ({ check, setCheck }) => {
-  const onChange = () => {
-    setCheck(!check)
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setCheck(e.target.checked)
   };
   return (
     <label className={classes.switch}>
