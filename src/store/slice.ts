20f906a5a74f8f46fdf77c74e721a3ce6d46513e
ElkaMaclaud <elkamaclaud@gmail.com>
2023-12-12 15:42:56 +0300

    Аound a mistake in change favorite and fixed it

--- src/store/slice.ts
+++ src/store/slice.ts
@@ -218,97 +218,90 @@ export const GET_SALE_GOODS = createAsyncThunk<
   IGoods[],
   undefined,
   { rejectValue: string; state: RootState }
->(
-  "page/GET_SALE_GOODS",
-  async (_, { rejectWithValue, getState }) => {
-    try {
-      const state = getState();
-      const {
-        data: {
-          user: { basket, favorite },
-        },
-      } = state.page;
-      const success = true;
-      if (success) {
-        const goodsWithDetails = goods
-          .filter((item) => item.sale)
-          .map((good: IGoods) => {
-            const findGood = basket.find(
-              (goodFind: IGoods) => goodFind.id === good.id
-            );
-            const favoriteGood = favorite.find(
-              (likeGood: IGoods) => likeGood.id === good.id
-            );
-            return {
-              ...good,
-              count: findGood?.count || 0,
-              choice: findGood?.choice || false,
-              favorite: favoriteGood?.favorite || false,
-            };
-          });
-        const response = await new Promise<IGoods[]>((resolve) => {
-          setTimeout(() => {
-            resolve(goodsWithDetails);
-          }, 0);
+>("page/GET_SALE_GOODS", async (_, { rejectWithValue, getState }) => {
+  try {
+    const state = getState();
+    const {
+      data: {
+        user: { basket, favorite },
+      },
+    } = state.page;
+    const success = true;
+    if (success) {
+      const goodsWithDetails = goods
+        .filter((item) => item.sale)
+        .map((good: IGoods) => {
+          const findGood = basket.find(
+            (goodFind: IGoods) => goodFind.id === good.id
+          );
+          const favoriteGood = favorite.find(
+            (likeGood: IGoods) => likeGood.id === good.id
+          );
+          return {
+            ...good,
+            count: findGood?.count || 0,
+            choice: findGood?.choice || false,
+            favorite: favoriteGood?.favorite || false,
+          };
         });
-        return response;
-      } else {
-        throw new Error("GET_SALE_GOODS failed");
-      }
-    } catch (error) {
-      return rejectWithValue(`${error}`);
+      const response = await new Promise<IGoods[]>((resolve) => {
+        setTimeout(() => {
+          resolve(goodsWithDetails);
+        }, 0);
+      });
+      return response;
+    } else {
+      throw new Error("GET_SALE_GOODS failed");
     }
+  } catch (error) {
+    return rejectWithValue(`${error}`);
   }
-);
-
+});
 
 export const GET_DISCOUNT_GOODS = createAsyncThunk<
   IGoods[],
   undefined,
   { rejectValue: string; state: RootState }
->(
-  "page/GET_DISCOUNT_GOODS",
-  async (_, { rejectWithValue, getState }) => {
-    try {
-      const state = getState();
-      const {
-        data: {
-          user: { basket, favorite },
-        },
-      } = state.page;
-      const success = true;
+>("page/GET_DISCOUNT_GOODS", async (_, { rejectWithValue, getState }) => {
+  try {
+    const state = getState();
+    const {
+      data: {
+        user: { basket, favorite },
+      },
+    } = state.page;
+    const success = true;
 
-      if (success) {
-        const goodsWithDetails = goods
-          .filter((item) => item.sale)
-          .map((good: IGoods) => {
-            const findGood = basket.find(
-              (goodFind: IGoods) => goodFind.id === good.id
-            );
-            const favoriteGood = favorite.find(
-              (likeGood: IGoods) => likeGood.id === good.id
-            );
-            return {
-              ...good,
-              count: findGood?.count || 0,
-              choice: findGood?.choice || false,
-              favorite: favoriteGood?.favorite || false,
-            };
-          });
-        const response = await new Promise<IGoods[]>((resolve) => {
-          setTimeout(() => {
-            resolve(goodsWithDetails);
-          }, 0);
+    if (success) {
+      const goodsWithDetails = goods
+        .filter((item) => item.sale)
+        .map((good: IGoods) => {
+          const findGood = basket.find(
+            (goodFind: IGoods) => goodFind.id === good.id
+          );
+          const favoriteGood = favorite.find(
+            (likeGood: IGoods) => likeGood.id === good.id
+          );
+          return {
+            ...good,
+            count: findGood?.count || 0,
+            choice: findGood?.choice || false,
+            favorite: favoriteGood?.favorite || false,
+          };
         });
-        return response;
-      } else {
-        throw new Error("GET_DISCOUNT_GOODS failed");
-      }
-    } catch (error) {
-      return rejectWithValue(`${error}`);
+      const response = await new Promise<IGoods[]>((resolve) => {
+        setTimeout(() => {
+          resolve(goodsWithDetails);
+        }, 0);
+      });
+      return response;
+    } else {
+      throw new Error("GET_DISCOUNT_GOODS failed");
     }
+  } catch (error) {
+    return rejectWithValue(`${error}`);
   }
-);
+});
 
 // export const GET_GOOD = createAsyncThunk<
 //   IGoods,
@@ -600,10 +593,19 @@ export const CHANGE_FAVORITE_GOOD = createAsyncThunk<
                     (good: IGoods) => good.id !== good_id
                   )
                 : [
-                    {
-                      ...goods.find((good: IGoods) => good.id === good_id),
-                      favorite: true,
-                    },
+                    getState().page.data.user.basket.find(
+                      (good: IGoods) => good.id === good_id
+                    )
+                      ? {
+                          ...getState().page.data.user.basket.find(
+                            (good: IGoods) => good.id === good_id
+                          ),
+                          favorite: true,
+                        }
+                      : {
+                          ...goods.find((good: IGoods) => good.id === good_id),
+                          favorite: true,
+                        },
                     ...getState().page.data.user.favorite,
                   ],
             });
