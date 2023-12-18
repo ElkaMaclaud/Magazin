9620e8b2d6530e0d4b180b3d981e3a574324880a
ElkaMaclaud <elkamaclaud@gmail.com>
2023-12-18 15:43:40 +0300

    h

--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -18,9 +18,10 @@ const ListItem: FC<{ list: IListCategory[]; small?: boolean; setFoo?: Dispatch<S
     <ul className={small ? classes.wrapperListSmall : classes.wrapperList}>
       {list.map((item) => {
         const key = keyGenerate();
-        return (
+        if (item.link) {
+          return (
           <Link
-          to={`../category/${item.link}`}
+            to={`../category/${item.link}`}
             onMouseOver={() => setHover(item)}
             key={key}
             className={
@@ -37,7 +38,25 @@ const ListItem: FC<{ list: IListCategory[]; small?: boolean; setFoo?: Dispatch<S
             </div>
             <span className={classes.arrow}></span>
           </Link>
-        );
+        )}
+        return (
+          <div
+            key={key}
+            className={
+              small
+                ? classes.itemSmall
+                : hover === item
+                ? classes.hoverItem
+                : classes.item
+            }
+          >
+            <div className={classes.itemInfo}>
+              <div>{item.icon}</div>
+              {item.name}
+            </div>
+            <span className={classes.arrow}></span>
+          </div>
+        )     
       })}
     </ul>
   );
