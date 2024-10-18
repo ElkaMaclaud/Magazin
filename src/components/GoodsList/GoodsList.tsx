bf02c764910418dc57310a63662c0fbebb900a39
ElkaMaclaud <Elkamaclaud@gmail.com>
2024-10-18 20:19:53 +0300

    Added lazy loading to products rendering component

--- src/components/GoodsList/GoodsList.tsx
+++ src/components/GoodsList/GoodsList.tsx
@@ -2,7 +2,6 @@ import React, {
   CSSProperties,
   FC,
   ReactNode,
-  UIEvent,
   useEffect,
   useState,
 } from "react";
@@ -14,14 +13,35 @@ const GoodsList: FC<{
   icon?: ReactNode;
   orientationVertical?: boolean;
 }> = ({ data, icon, orientationVertical }) => {
-  const [goods, setGoods] = useState(data.slice(0, 6));
+  const [index, setIndex] = useState(10);
+  const [goods, setGoods] = useState(data.slice(0, index));
+  const [lastScroll, setLastScroll] = useState(0);
+
   useEffect(() => {
-    setGoods(data.slice(0, 20));
-  }, [data]);
-  const handleScroll = (e: UIEvent<HTMLDivElement>) => {
-    const scroll = e.currentTarget.scrollTop
-    console.log("//////////////////", scroll)
+    setGoods(data.slice(0, index));
+    if (data.length && index > data.length) {
+      setLastScroll(-1);
+    }
+  }, [data, index]);
+
+  const handleScroll = () => {
+    const scroll = window.scrollY;
+    if (scroll - lastScroll > 30 && lastScroll >= 0) {
+      setLastScroll(scroll);
+      setIndex((prev) => (prev += 1));
+    }
   };
+
+  useEffect(() => {
+    if (lastScroll >= 0) {
+      document.addEventListener("scroll", handleScroll);
+    }
+    return () => {
+      document.removeEventListener("scroll", handleScroll);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   const style: CSSProperties = orientationVertical
     ? {
         display: "flex",
@@ -33,7 +53,7 @@ const GoodsList: FC<{
       }
     : {};
   return (
-    <div style={style} onScroll={handleScroll}>
+    <div style={style}>
       {goods.map((item) => {
         return (
           <WideCard
